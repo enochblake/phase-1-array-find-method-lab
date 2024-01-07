e3de25a540dc0c417081f4b562367ef7888d6fb0
Enoch Kibet <enochblake9@gmail.com>
2024-01-07 17:02:43 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function superbowlWin(array) {
+    const winningGame = array.find(game => game.result === "W");
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  // Example usage:
+  const games = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+  ];
+  
+  console.log(superbowlWin(games)); // Output: 2021
+  
